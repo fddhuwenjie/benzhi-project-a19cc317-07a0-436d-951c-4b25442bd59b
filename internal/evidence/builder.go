9025aa3469263bec48eb855e50df830807d27a62
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Green result for a19cc317-07a0-436d-951c-4b25442bd59b-bug-3

--- internal/evidence/builder.go
+++ internal/evidence/builder.go
@@ -31,13 +31,43 @@ func (s *Service) cached(id string) (Package, bool) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	p, ok := s.cache[id]
-	return p, ok
+	if !ok {
+		return Package{}, false
+	}
+	c, err := clonePackage(p)
+	if err != nil {
+		return Package{}, false
+	}
+	return c, true
 }
 
 func (s *Service) remember(id string, p Package) {
+	cloned, err := clonePackage(p)
+	if err != nil {
+		// 一份已序列化成功并落盘的证据包必然可被 JSON 复制；
+		// 若克隆失败说明证据包本身已不可用，不写入缓存以免污染后续构建。
+		return
+	}
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.cache[id] = p
+	s.cache[id] = cloned
+}
+
+// clonePackage 返回证据包的深度副本。
+// Package 内部的 Trial（含 RejectionReasons、Observations、Review、Permit 等
+// 引用类型字段）与 AuditEvents 切片共享底层数组，调用方修改返回值会直接污染
+// 缓存。这里通过 JSON 序列化/反序列化完整复制其可达对象图，确保后续构建始终返回
+// 仓库冻结时的事实，证据摘要和语义对账不受其他调用方状态影响。
+func clonePackage(p Package) (Package, error) {
+	data, err := json.Marshal(p)
+	if err != nil {
+		return Package{}, err
+	}
+	var c Package
+	if err := json.Unmarshal(data, &c); err != nil {
+		return Package{}, err
+	}
+	return c, nil
 }
 
 func calculate(t domain.ClearanceTrial, events []domain.AuditEvent) string {
